319d751db633b0cf195e28977ee660b1e02fa975
Julio Viquez <julio.a.viquez@gmail.com>
2026-06-03 10:00:39 -0600

    leccion_4

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,1 +1,15 @@
-vista superior
+<!DOCTYPE html>
+<html lang="en">
+<!-- INICIO DE LA PARTE SUPERIOR DE LA PAGINA... CESAR SEGURA -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-compatible" conte="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/main.css"
+    <title>Tercera 1</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Vista Superior</h1>
+            <p>Esta es la vista superior del projecto.</p>
+        </div>
